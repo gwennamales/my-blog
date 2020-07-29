b2b84ac52042db313d2f4e591427800cccba650a
Gwenna Males <boukaka2002@hotmail.com>
2020-07-29 14:44:52 -0400

    initial commit

---
+++ src/build/precache-manifest.885b350cd696bd904d2e3a42081529ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbfe21bce906f0ad77417fdf5fac283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf42482e73e8a6ad5d5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a744b7b29ed07699aa8d",
+    "url": "/static/js/2.c4b4c507.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4b4c507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf42482e73e8a6ad5d5",
+    "url": "/static/js/main.93c989d1.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
